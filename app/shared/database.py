feec07f57e21ad027acf9ced9b5419388cd058f3
Neto test <eprohacev@gmail.com>
2026-04-24 23:54:36 +0300

    правка файла конфигурации

--- app/shared/database.py
+++ app/shared/database.py
@@ -1,11 +1,17 @@
 from typing import Annotated
+import os
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from fastapi import Depends
 
-engine = create_async_engine('sqlite+aiosqlite:///leads.db')
+if os.path.exists('/data'):
+    db_path = '/data/leads.db'
+else:
+    db_path = 'leads.db'  # локально для разработки
+
+engine = create_async_engine(f'sqlite+aiosqlite:///{db_path}')
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
